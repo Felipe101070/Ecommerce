70d584d448a10cc596c3bd249260cfeefc6248c9
Felipe Araujo <felipej.araujo@gmail.com>
2022-04-02 19:21:28 -0300

    add produrct

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -6,7 +6,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+from pathlib import Path
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
