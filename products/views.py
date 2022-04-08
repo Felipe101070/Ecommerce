f775998271c5f787c5aa90af23491f4a56e05546
Felipe Araujo <felipej.araujo@gmail.com>
2022-04-08 20:08:57 -0300

    add novas funcionalidades

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 
@@ -8,17 +9,18 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/list.html"
     
-    #def get_context_data(self, *args, **kwargs):
-    #    context = super(ProductListView, self).get_context_data(*args, **kwargs)
-    #    print(context)
-    #    return context
-    
 class ProductDetailView(DetailView):
-    #traz todos os produtos do banco de dados sem filtrar nada 
-    queryset = Product.objects.all()
+    #traz todos os produtos do banco de dados sem filtrar nada
     template_name = "products/detail.html"
     
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
+    
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Esse produto não existe!")
+        return instance
