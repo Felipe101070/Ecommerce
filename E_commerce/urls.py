b88915381feac3985dbfecbc7af38a16f018e9f8
Felipe Araujo <felipej.araujo@gmail.com>
2022-04-08 16:27:36 -0300

    Update add novas funcionalidades

--- E_commerce/urls.py
+++ E_commerce/urls.py
@@ -19,16 +19,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from products.views import ProductListView
+from products.views import ProductListView, ProductDetailView
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
 	path('', home_page),
 	path('about/', about_page),
 	path('contact/', contact_page),
-    path('login/', login_page),
-    path('products/', ProductListView.as_view()),
+    path('login/', login_page), 
     path('register/', register_page),
+    path('products/', ProductListView.as_view()),
+    path('products/<int:pk>', ProductDetailView.as_view()),
 	path('admin/', admin.site.urls),
 ]
 
